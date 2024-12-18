1388d40ea6ca81084bd992724201ac3bd2d00ae7
Christopher Green <cgreen@saguaro.space>
2024-12-18 13:31:08 -0700

    Cleaned up Neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,4 @@
+-- cgreen's Neovim config
 vim.cmd('filetype plugin indent on')
 
 -- Options
@@ -55,22 +56,41 @@ vim.keymap.set('n', 'd<Space>', ':lua MiniBufremove.delete()<CR>')
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>') -- In terminal mode, use Esc to go back to normal mode
 vim.keymap.set('t', '<C-v><Esc>', '<Esc>') -- Use C-v Esc to send Esc in terminal mode
 
+-- Colorscheme
 vim.cmd.colorscheme('retrobox')
 
+-- Override highlight groups
+vim.api.nvim_create_autocmd({'Colorscheme'}, {
+  group = vim.api.nvim_create_augroup('Mini Fixes', { clear = true }),
+  callback = function()
+    -- vim.cmd('highlight link MiniPickMatchCurrent Cursor')
+    -- vim.cmd('highlight link MiniCursorword EndofBuffer')
+    vim.cmd('highlight link MiniIndentscopeSymbol LineNr')
+  end
+})
+
+-- Delete buffer without closing window
 vim.api.nvim_create_user_command('Clear', function()
     vim.cmd("bufdo bwipeout")
 end, {})
 
+-- Biome (for JavaScript formatting)
+vim.api.nvim_create_augroup('biome', { clear = true })
+vim.api.nvim_create_autocmd('FileType', {
+  group = 'biome',
+  pattern = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'json' },
+  callback = function()
+    vim.bo.formatprg = 'biome format --stdin-file-path=%'
+  end,
+})
 
--- Mini (colorscheme, picker, pairs, filebrowser)
+-- Initialize mini.nvim; automatically clone it if necessary...
 local path_package = vim.fn.stdpath('data') .. '/site'
 local mini_path = path_package .. '/pack/deps/start/mini.nvim'
 if not vim.loop.fs_stat(mini_path) then
   vim.cmd('echo "Installing `mini.nvim`" | redraw')
   local clone_cmd = {
     'git', 'clone', '--filter=blob:none',
-    -- Uncomment next line to use 'stable' branch
-    -- '--branch', 'stable',
     'https://github.com/echasnovski/mini.nvim', mini_path
   }
   vim.fn.system(clone_cmd)
@@ -80,6 +100,7 @@ end
 require('mini.deps').setup({ path = { package = path_package } })
 
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
+-- ...end. Now we are ready to use use mini.nvim.
 
 now(function()
     require('mini.basics').setup()
@@ -89,50 +110,30 @@ now(function()
     require('mini.cursorword').setup()
     require('mini.diff').setup()
     require('mini.extra').setup()
+    require('mini.files').setup()
     require('mini.fuzzy').setup()
     require('mini.git').setup()
     require('mini.hipatterns').setup()
+    require('mini.icons').setup({ style = 'ascii' })
+    require('mini.indentscope').setup({ symbol = "🞍" })
     require('mini.map').setup()
     require('mini.notify').setup()
     require('mini.pairs').setup()
+    require('mini.pick').setup()
+    require('mini.sessions').setup()
     require('mini.starter').setup()
+    require('mini.statusline').setup({ use_icons = false })
     require('mini.surround').setup()
     require('mini.tabline').setup()
-
-    require('mini.statusline').setup({ use_icons = false })
-
-    require('mini.icons').setup({
-        style = 'ascii'
-    })
-
-    require('mini.indentscope').setup({
-        symbol = "🞍"
-    })
-
-    require('mini.files').setup()
-    require('mini.pick').setup()
-
-    local hipatterns = require('mini.hipatterns')
-
-    hipatterns.setup({
+    require('mini.hipatterns').setup({
       highlighters = {
         fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
         hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
         todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
         note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
 
-        hex_color = hipatterns.gen_highlighter.hex_color(),
+        hex_color = require('mini.hipatterns').gen_highlighter.hex_color(),
       },
-    })
-
-
-    vim.api.nvim_create_autocmd({'Colorscheme'}, {
-        group = vim.api.nvim_create_augroup('Mini Fixes', { clear = true }),
-        callback = function()
-            -- vim.cmd('highlight link MiniPickMatchCurrent Cursor')
-            -- vim.cmd('highlight link MiniCursorword EndofBuffer')
-            vim.cmd('highlight link MiniIndentscopeSymbol LineNr')
-        end
     })
 end)
 
@@ -149,7 +150,7 @@ later(function()
   add("neovim/nvim-lspconfig")
 
   -- JavaScript/Typescript language server with Vue plugin
-  require'lspconfig'.ts_ls.setup {
+  require('lspconfig').ts_ls.setup {
     init_options = {
       plugins = {
         {
@@ -167,7 +168,7 @@ later(function()
   }
 
   -- Vue language server
-  require'lspconfig'.volar.setup {
+  require('lspconfig').volar.setup {
     init_options = {
       typescript = {
         tsdk = '/Users/cgreen/.npm-global/lib/node_modules/typescript/lib'
@@ -179,7 +180,6 @@ end)
 --
 -- Treesitter
 --
--- npm install -g
 -- Don't forget to install the tree-sitter CLI
 -- Treesitter (highlight, edit, navigate code)
 later(function()
@@ -198,14 +198,4 @@ later(function()
         },
     })
 end)
-
--- Biome (for JavaScript formatting)
-vim.api.nvim_create_augroup('biome', { clear = true })
-vim.api.nvim_create_autocmd('FileType', {
-  group = 'biome',
-  pattern = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'json' },
-  callback = function()
-    vim.bo.formatprg = 'biome format --stdin-file-path=%'
-  end,
-})
 
