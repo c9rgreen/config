9918c422a6321de58e1d434906d500baa22d1ccc
Christopher Green <cgreen@saguaro.space>
2025-01-23 08:22:12 -0700

    [fish] Sets up fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,17 @@
+# Brew
+eval (/opt/homebrew/bin/brew shellenv)
+
+# Variables
+set -U fish_greeting # disable fish greeting
+set -U fish_key_bindings fish_vi_key_bindings
+set -Ux EDITOR nvim
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Direnv
+direnv hook fish | source
+
+# Helpful docs
+# https://www.joshmedeski.com/posts/set-up-fish-the-user-friendly-interactive-shell/
