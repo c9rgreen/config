2037f6df4749c279c5bebf555401ea04b7943d09
Christopher Green <cgreen@saguaro.space>
2025-01-24 09:10:33 -0700

    [neovim] Adds back format on save

--- nvim/init.lua
+++ nvim/init.lua
@@ -94,6 +94,20 @@ vim.api.nvim_create_autocmd({ 'Colorscheme' }, {
       vim.cmd('highlight link MiniPickMatchCurrent TabLineSel')
    end
 })
+
+-- Format on save
+-- https://www.mitchellhanberg.com/modern-format-on-save-in-neovim/
+vim.api.nvim_create_autocmd("LspAttach", {
+   group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+   callback = function(args)
+      vim.api.nvim_create_autocmd("BufWritePre", {
+         buffer = args.buf,
+         callback = function()
+            vim.lsp.buf.format { async = false, id = args.data.client_id }
+         end,
+      })
+   end
+})
 -- }}}
 
 -- Mini {{{
