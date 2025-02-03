b445a5ecbbe624faac6af885f56bcc9ba9b74c79
Christopher Green <cgreen@saguaro.space>
2025-02-03 10:22:33 -0700

    [Neovim] Use fish, fix Monokai Pro float colors, remove terminal mode mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,6 +15,7 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldtext = ""
 vim.opt.wildignorecase = true
+vim.opt.shell = "fish"
 
 -- Ghostty.app
 vim.opt.runtimepath:append("/Applications/Ghostty.app/Contents/Resources/vim/vimfiles")
@@ -24,7 +25,6 @@ vim.opt.runtimepath:append("/opt/homebrew/share/lilypond/2.24.3/vim")
 -- }}}
 
 -- Variables {{{
--- NetRW
 vim.g.netrw_liststyle = 3
 vim.g.netrw_banner = 0
 -- }}}
@@ -36,14 +36,6 @@ vim.api.nvim_create_autocmd({ 'TermOpen' }, {
    callback = function()
       vim.opt.number = false
       vim.opt.relativenumber = false
-   end
-})
-
--- Highlight groups
-vim.api.nvim_create_autocmd({ 'Colorscheme' }, {
-   group = vim.api.nvim_create_augroup('Mini', { clear = true }),
-   callback = function()
-      vim.cmd('highlight link MiniPickMatchCurrent TabLineSel')
    end
 })
 
@@ -78,7 +70,9 @@ vim.api.nvim_create_autocmd("OptionSet", {
 require("mini")
 require("lsp")
 require("treesitter")
-require("monokai-pro").setup()
+require("monokai-pro").setup({
+   background_clear = { "float_win" }
+})
 -- }}}
 
 -- Mappings {{{
@@ -92,6 +86,4 @@ vim.keymap.set('n', '<leader>h', ':lua MiniPick.builtin.help()<CR>')
 vim.keymap.set('n', '<leader>l', ':lua MiniExtra.pickers.lsp({ scope = "document_symbol" })<CR>')
 vim.keymap.set('n', '<leader>o', ':lua MiniDiff.toggle_overlay()<CR>')
 vim.keymap.set('n', 'd<Space>', ':lua MiniBufremove.delete()<CR>')
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>') -- In terminal mode, use Esc to go back to normal mode
-vim.keymap.set('t', '<C-v><Esc>', '<Esc>')  -- Use C-v Esc to send Esc in terminal mode
 -- }}}
