63977063d8374ad3e18aab008660d7e066f66a60
Christopher Green <cgreen@saguaro.space>
2025-03-03 12:40:56 -0700

    More succinct depcruise command

--- git/mr-deps.sh
+++ git/mr-deps.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-FILES=$(git diff --name-only $(git merge-base HEAD "$REVIEW_BASE") \
-  | tr '\n' ' ' \
-  | sed 's/ /|/g; s/|*$//; s/\./\\./g; s/\//\\\//g')
-
-
-npx depcruise \
-  --include-only "$FILES" \
-  --output-type dot src \
+npx depcruise src \
+  --affected \
+  $REVIEW_BASE \
+  --output-type dot \
   | dot -T svg | chafa
 
