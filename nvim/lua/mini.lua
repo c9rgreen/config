21025f3101ce0ba88aba6b2c34af9fa6ec83aa60
Christopher Green <cgreen@saguaro.space>
2025-03-27 10:24:20 -0700

    [Neovim] Removes Fern

--- nvim/lua/mini.lua
+++ nvim/lua/mini.lua
@@ -12,7 +12,6 @@ end
 
 require('mini.align').setup()
 require('mini.basics').setup()
-require('mini.bracketed').setup()
 require('mini.bufremove').setup()
 require('mini.completion').setup()
 require('mini.cursorword').setup()
@@ -67,50 +66,6 @@ require('mini.snippets').setup({
       -- Load snippets based on current language by reading files from
       -- "snippets/" subdirectories from 'runtimepath' directories.
       require('mini.snippets').gen_loader.from_lang(),
-   },
-})
-
-require('mini.clue').setup({
-   triggers = {
-      -- Leader triggers
-      { mode = 'n', keys = '<Leader>' },
-      { mode = 'x', keys = '<Leader>' },
-
-      -- Built-in completion
-      { mode = 'i', keys = '<C-x>' },
-
-      -- `g` key
-      { mode = 'n', keys = 'g' },
-      { mode = 'x', keys = 'g' },
-
-      -- Marks
-      { mode = 'n', keys = "'" },
-      { mode = 'n', keys = '`' },
-      { mode = 'x', keys = "'" },
-      { mode = 'x', keys = '`' },
-
-      -- Registers
-      { mode = 'n', keys = '"' },
-      { mode = 'x', keys = '"' },
-      { mode = 'i', keys = '<C-r>' },
-      { mode = 'c', keys = '<C-r>' },
-
-      -- Window commands
-      { mode = 'n', keys = '<C-w>' },
-
-      -- `z` key
-      { mode = 'n', keys = 'z' },
-      { mode = 'x', keys = 'z' },
-   },
-
-   clues = {
-      -- Enhance this by adding descriptions for <Leader> mapping groups
-      require('mini.clue').gen_clues.builtin_completion(),
-      require('mini.clue').gen_clues.g(),
-      require('mini.clue').gen_clues.marks(),
-      require('mini.clue').gen_clues.registers(),
-      require('mini.clue').gen_clues.windows(),
-      require('mini.clue').gen_clues.z(),
    },
 })
 
@@ -181,7 +136,3 @@ add("kristijanhusak/vim-dadbod-completion")
 -- Emmet
 add("mattn/emmet-vim")
 
--- Trees
-add("lambdalisue/vim-fern")
-add("lambdalisue/vim-fern-renderer-nerdfont")
-add("lambdalisue/vim-nerdfont")
