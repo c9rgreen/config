d91aea78860a4964d12080e7429e7cc2807bb4a9
Christopher Green <cgreen@saguaro.space>
2025-04-18 06:11:29 -0700

    Removes unused configs

--- tower/nvim.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-# This is a custom difftool for Tower
-# https://www.git-tower.com/help/guides/integration/custom-diff-tools/mac
-#
-# $1: $LOCAL
-# $2: $REMOTE
-/opt/homebrew/bin/nvim -d "$1" "$2"
