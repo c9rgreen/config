7542ca1127bd14275cc65a56cd65db825c27d0ca
Christopher Green <cgreen@saguaro.space>
2025-08-06 12:14:11 -0700

    [Ghostty] Adds minisummer light and dark themes

--- nvim/init.lua
+++ nvim/init.lua
@@ -92,6 +92,14 @@ require('mini.snippets').setup({
    },
 })
 
+
+local hipatterns = require('mini.hipatterns')
+hipatterns.setup({
+   highlighters = {
+      hex_color = hipatterns.gen_highlighter.hex_color(),
+   }
+})
+
 vim.cmd.colorscheme("minisummer")
 
 -- Treesitter
