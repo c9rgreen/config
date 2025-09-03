10a98a9d67793982da2290ea5ea16ac9dea242a2
Christopher Green <cgreen@saguaro.space>
2025-09-03 09:47:18 -0700

    [Fish] Adds OrbStack integration

--- fish/config.fish
+++ fish/config.fish
@@ -52,3 +52,7 @@ if type -q npm
     set -a PATH "$HOME/.npm-global/bin"
 end
 
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
