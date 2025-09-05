1c849826321833fa098619092d0d7232cdddbfc4
Christopher Green <cgreen@saguaro.space>
2025-09-05 14:18:31 -0700

    [Fish] Changes comment above eza alias

--- fish/config.fish
+++ fish/config.fish
@@ -53,13 +53,13 @@ if type -q npm
 end
 
 #
-# Aliases
+# Eza
 #
 if type -q eza
     alias ls "eza --icons"
 end
 
-
+#
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
