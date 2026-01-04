faa5376f94c4a7d6380b1bb0e900e0bcf94b1153
Christopher Green <cgreen@saguaro.space>
2026-01-04 14:55:14 -0700

    [Ghostty, Neovim] Switch to Modus theme

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -172,3 +172,18 @@ add("zk-org/zk-nvim")
 require("zk").setup({
    picker = "minipick"
 })
+
+--
+-- Modus colorscheme
+-- 
+add("miikanissi/modus-themes.nvim")
+
+require("modus-themes").setup({
+   -- variant = "tinted",
+   on_highlights = function(highlight, color)
+      highlight.MiniCursorword = { bg = color.bg_yellow_subtle, fg = color.fg_alt }
+      highlight.MiniCursorwordCurrent = { bg = color.bg_yellow_nuanced }
+   end,
+})
+
+vim.cmd.colorscheme("modus")
