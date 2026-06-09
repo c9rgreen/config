da62005162954ce0f09becf1fc956d1959aa9b6a
Christopher Green <cgreen@internet2.edu>
2026-06-09 09:45:56 -0700

    Auto install Treesitter parsers; Adds Mason, Mini.snippets and cursorword

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,6 +43,8 @@ vim.pack.add({
    'https://github.com/nvim-treesitter/nvim-treesitter',
    'https://github.com/nvim-treesitter/nvim-treesitter-context',
    'https://github.com/neovim/nvim-lspconfig',
+   'https://github.com/mason-org/mason.nvim',
+   'https://github.com/mason-org/mason-lspconfig.nvim',
    'https://github.com/nvim-mini/mini.nvim',
 })
 
@@ -58,52 +60,42 @@ require('mini.pick').setup()
 require('mini.statusline').setup()
 require('mini.icons').setup()
 require('mini.tabline').setup()
+require('mini.snippets').setup()
+require('mini.cursorword').setup()
 
 vim.cmd.colorscheme('minisummer')
 
-local languages = {
-   'bash',
-   'caddy',
-   'css',
-   'eex',
-   'elixir',
-   'gitcommit',
-   'heex',
-   'html',
-   'hurl',
-   'javascript',
-   'jinja',
-   'json',
-   'julia',
-   'liquid',
-   'lua',
-   'markdown',
-   'markdown_inline',
-   'mermaid',
-   'python',
-   'rst',
-   'sql',
-   'vim',
-   'vimdoc',
-   'vue',
-   'yaml',
-}
+-- Install treesitter parsers on demand, driven by the buffer's filetype
+local ts = require('nvim-treesitter')
+local available = ts.get_available()
 
-require('nvim-treesitter').install(languages)
+local function start(buf, lang)
+   vim.treesitter.start(buf, lang)
+   vim.wo[0][0].foldmethod = 'expr'
+   vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+   vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+end
 
 vim.api.nvim_create_autocmd('FileType', {
-   pattern = languages,
-   callback = function()
-      vim.treesitter.start()
-      vim.wo.foldmethod = 'expr'
-      vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+   callback = function(args)
+      local lang = vim.treesitter.language.get_lang(args.match) or args.match
+      if not vim.list_contains(available, lang) then
+         return
+      end
+      if vim.list_contains(ts.get_installed(), lang) then
+         start(args.buf, lang)
+      else
+         ts.install({ lang }):await(vim.schedule_wrap(function(err)
+            if not err then
+               start(args.buf, lang)
+            end
+         end))
+      end
    end,
 })
 
 -- Display the context of the cursor in a sticky header
 require('treesitter-context').setup()
-
 
 vim.lsp.enable({
    'cssls',
@@ -121,5 +113,14 @@ vim.lsp.enable({
    'yamlls'
 })
 
+-- Mason installs LSP servers
+require('mason').setup()
+
+-- Automatically install the language servers configured by vim.lsp.enable
+require('mason-lspconfig').setup({
+   ensure_installed = vim.tbl_keys(vim.lsp._enabled_configs),
+})
+
 -- New UI
 require('vim._core.ui2').enable({})
+
