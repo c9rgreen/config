f59e8b60bc85d66a37f54ccbedefafefaea4fcae
Christopher Green <cgreen@saguaro.space>
2026-06-09 08:58:17 -0700

    [Neovim] Removes Mini, Mason, plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,6 +8,29 @@ vim.opt.diffopt:append('vertical,iwhiteall,algorithm:histogram')
 vim.opt.splitright = true
 vim.opt.number = false
 vim.opt.path:append { '**' }
+vim.opt.wildoptions:append('fuzzy')
+vim.opt.completeopt = 'menu,menuone,noselect,popup'
+vim.opt.autocomplete = true
+vim.opt.signcolumn = 'yes'
+
+-- LSP completion
+vim.api.nvim_create_autocmd("LspAttach", {
+   group = vim.api.nvim_create_augroup("lsp_completion", { clear = true }),
+   callback = function(args)
+      local client_id = args.data.client_id
+      if not client_id then
+         return
+      end
+
+      local client = vim.lsp.get_client_by_id(client_id)
+      if client and client:supports_method("textDocument/completion") then
+         -- Enable native LSP completion for this client + buffer
+         vim.lsp.completion.enable(true, client_id, args.buf, {
+            autotrigger = true, -- auto-show menu
+         })
+      end
+   end,
+})
 
 -- Use virtual text for diagnostics
 vim.diagnostic.config({ virtual_text = true })
@@ -37,118 +60,11 @@ vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
 vim.g.netrw_browse_split = 4
 
---
--- Mini.nvim
--- https://nvim-mini.org/mini.nvim/
---
-local path_package = vim.fn.stdpath('data') .. '/site'
-local mini_path = path_package .. '/pack/deps/start/mini.nvim'
-if not vim.loop.fs_stat(mini_path) then
-   vim.cmd("echo 'Installing `mini.nvim`' | redraw")
-   local clone_cmd = {
-      'git', 'clone', '--filter=blob:none',
-      'https://github.com/nvim-mini/mini.nvim', mini_path
-   }
-   vim.fn.system(clone_cmd)
-   vim.cmd('packadd mini.nvim | helptags ALL')
-end
-
-require('mini.deps').setup({ path = { package = path_package } })
-
-local add = MiniDeps.add
-
-require('mini.align').setup()
-require('mini.pairs').setup()
-require('mini.basics').setup()
-require('mini.bufremove').setup()
-require('mini.clue').setup()
-require('mini.cmdline').setup()
-require('mini.comment').setup()
-require('mini.completion').setup()
-require('mini.cursorword').setup()
-require('mini.extra').setup()
-require('mini.files').setup({ options = { use_as_default_explorer = false } })
-require('mini.fuzzy').setup()
-require('mini.git').setup()
-require('mini.icons').setup()
-require('mini.indentscope').setup()
-require('mini.keymap').setup()
-require('mini.map').setup()
-require('mini.move').setup()
-require('mini.notify').setup()
-require('mini.pick').setup()
-require('mini.sessions').setup()
-require('mini.starter').setup()
-require('mini.statusline').setup()
-require('mini.surround').setup()
-require('mini.tabline').setup()
-require('mini.trailspace').setup()
-require('mini.visits').setup()
-
-
-require('mini.snippets').setup({
-   snippets = {
-      require('mini.snippets').gen_loader.from_file('~/.config/nvim/snippets/global.json'),
-      require('mini.snippets').gen_loader.from_lang(),
-   },
-})
-
-local hipatterns = require('mini.hipatterns')
-hipatterns.setup({
-   highlighters = {
-      hex_color = hipatterns.gen_highlighter.hex_color(),
-   }
-})
-
-local mini_diff = require('mini.diff')
-mini_diff.setup()
-
--- Commands
-local mini_extra = require('mini.extra')
-local mini_pick = require('mini.pick')
-local mini_commands = {
-   -- MiniExtra Pickers
-   Branches = function() mini_extra.pickers.git_branches() end,
-   Diagnostic = function() mini_extra.pickers.diagnostic() end,
-   DocumentSymbol = function() mini_extra.pickers.lsp({ scope = 'document_symbol' }) end,
-   Keymaps = function() mini_extra.pickers.keymaps() end,
-   Marks = function() mini_extra.pickers.marks() end,
-   Quickfix = function() mini_extra.pickers.list({ scope = 'quickfix' }) end,
-   WorkspaceSymbol = function() mini_extra.pickers.lsp({ scope = 'workspace_symbol' }) end,
-
-   -- MiniPick Builtin Pickers
-   Files = function() mini_pick.builtin.files() end,
-   Buffers = function() mini_pick.builtin.buffers() end,
-   Grep = function() mini_pick.builtin.grep_live() end,
-   Help = function() mini_pick.builtin.help() end,
-
-   -- Other Commands
-   Diff = function() mini_diff.toggle_overlay() end,
-   Map = function() MiniMap.toggle() end,
-}
-
-for name, func in pairs(mini_commands) do
-   vim.api.nvim_create_user_command(name, func, {})
-end
-
--- Mappings for Mini.Pick
-vim.keymap.set('n', '\\\\', ':lua MiniFiles.open()<CR>', { desc = 'File browser' })
-vim.keymap.set('n', '<leader><leader>', ':Pick buffers<CR>', { desc = 'Buffer picker' })
-vim.keymap.set('n', '<leader>-', ':Pick files<CR>', { desc = 'File picker' })
-vim.keymap.set('n', '<leader>/', ':Pick grep_live<CR>', { desc = 'Live grep' })
-vim.keymap.set('n', '<leader>k', ':DocumentSymbol<CR>', { desc = 'Document symbols' })
-vim.keymap.set('n', '<leader>d', function() mini_extra.pickers.diagnostic() end, { desc = 'Diagnostics' })
-vim.keymap.set('n', '<leader>h', function() mini_pick.builtin.help() end, { desc = 'Help' })
-vim.keymap.set('n', '<leader><BS>', function() require('mini.bufremove').delete() end, { desc = 'Delete' })
-
-
--- Colorscheme
-vim.cmd.colorscheme('minisummer')
-
--- Treesitter - syntax highlighting, among other things
-add({
-   source = 'nvim-treesitter/nvim-treesitter',
-   hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
+-- Packages
+vim.pack.add({
+   'https://github.com/nvim-treesitter/nvim-treesitter',
+   'https://github.com/nvim-treesitter/nvim-treesitter-context',
+   'https://github.com/neovim/nvim-lspconfig',
 })
 
 local languages = {
@@ -192,20 +108,8 @@ vim.api.nvim_create_autocmd('FileType', {
 })
 
 -- Display the context of the cursor in a sticky header
-add('nvim-treesitter/nvim-treesitter-context')
-
 require('treesitter-context').setup()
 
--- LSP servers
-add({
-   source = 'mason-org/mason-lspconfig.nvim',
-   depends = {
-      'neovim/nvim-lspconfig',
-      'mason-org/mason.nvim',
-   }
-})
-
-require('mason').setup()
 
 vim.lsp.enable({
    'cssls',
@@ -223,7 +127,5 @@ vim.lsp.enable({
    'yamlls'
 })
 
--- Automatically install LSP servers
-require('mason-lspconfig').setup({
-   ensure_installed = vim.tbl_keys(vim.lsp._enabled_configs),
-})
+-- New UI
+require('vim._core.ui2').enable({})
