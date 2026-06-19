5b1ab9b45c2a742b12da61fe97b88de32bd15e34
Christopher Green <cgreen@internet2.edu>
2026-06-18 17:35:13 -0700

    Set ZK environment variable

--- fish/config.fish
+++ fish/config.fish
@@ -79,3 +79,8 @@ end
 #
 alias chrome "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222 --user-data-dir=/tmp/chrome-profile-stable"
 
+#
+# Global ZK notebook location
+# https://github.com/zk-org/zk-nvim
+#
+set -gx ZK_NOTEBOOK_DIR "$HOME/ZK"
