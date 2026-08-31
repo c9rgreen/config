726c4ef8ec035570ef1e8380997bad115cbde98b
Christopher Green <cgreen@cgreen-dev.macc.ns.internet2.edu>
2026-08-31 19:16:56 +0000

    [Fish] Configures homebrew for Linux

--- fish/conf.d/0-homebrew.fish
+++ fish/conf.d/0-homebrew.fish
@@ -1,5 +1,11 @@
 # Homebrew
 # https://docs.brew.sh/Installation
-if test -x /opt/homebrew/bin/brew
-    eval (/opt/homebrew/bin/brew shellenv)
+#
+# Linuxbrew on Linux, /opt/homebrew on Apple Silicon. Sourced from conf.d so it
+# runs before config.fish and brew's bin lands ahead of the system package
+# manager's on PATH.
+if test -x /home/linuxbrew/.linuxbrew/bin/brew
+    /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
+else if test -x /opt/homebrew/bin/brew
+    /opt/homebrew/bin/brew shellenv | source
 end
